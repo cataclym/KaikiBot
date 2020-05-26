37897e53219348a7e643f43e432837d5fe44e329
cataclym <ole.kh@hotmail.com>
2020-05-26 19:00:41 +0200

    Added prefixes / and prefix slice lengths!

--- index.js
+++ index.js
@@ -4,18 +4,49 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Bot has finished booting sequence');
+	client.user.setActivity('Renai Circulation', { type: 'LISTENING'});
 });
 
 const { prefix } = config;
 
 
 client.on('message', message => {
+	const member = message.member;
 
+	if (member.hasPermission('ADMINISTRATOR')) {
+    switch(message.content.toUpperCase()) {
+		case 'KILLNEKO':
+			resetBot(message.channel);
+			break;
+	}
+	}
+	 
 	const msgcnt = message.content.toLowerCase();
-		
-	if (message.content.toLowerCase().startsWith(`${prefix}`)) {
-		message.channel.send('Hello,' + (msgcnt.slice(4)));
+	
+	var prefixes = ["i'm", "im", "i am"]
+	var starts = false;
+    var prefixLength = 0;
+    for( i = 0; i < prefixes.length; i++){
+    if (msgcnt.startsWith(prefixes[i])){
+    starts = true;
+    prefixLength = prefixes[i].length;
+    i = prefixes.length;
+  }
+}
+	if (starts) {
+	     //if (msgcnt.startsWith(`${prefix}`)) {
+		message.channel.send('Hello,' + (message.content.slice(prefixLength)));
+	   
 }
 });
+
+// Turn bot off (destroy), then turn it back on
+function resetBot(channel) {
+    // send channel a message that you're resetting bot [optional]
+	channel.send('Shutting down :(')
+	.then(() => console.log('Shutting down'))
+	.then(msg => process.exit(1));
+
+}
 
 client.login(config.token);
