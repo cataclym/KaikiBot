96c28b839ef85f73ca3fc4df929c4c671b161e90
cataclym <ole.kh@hotmail.com>
2020-05-30 21:53:29 +0200

    added the external variable file
    this is where you can customize when bot should add reactions and which reactions to use !
    And also set activity status also known as presence.

---
+++ commands/variables.js
@@ -0,0 +1,13 @@
+// Dont change
+const prefixes = ["i'm ", "im ", "i am ", "i’m "]; 
+// Change these to words the bot should add reactions to //// Keep a space after words //// You can add as many as you want
+const prefixes2 = ["word1 ", "word2 "]; 
+const emotenames = ["emotename1", "emotename2", "emotename3", "emotename4"];
+
+// Bot activity status
+const activityname = "Renai circulation" // Edit for song/game/media.
+const activitystatus = "LISTENING" // change to one of: WATCHING/LISTENING/PLAYING.
+
+
+
+module.exports = { prefixes, prefixes2, emotenames, activityname, activitystatus };
