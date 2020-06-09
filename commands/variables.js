620b4dbb4eb06ac92af8fa1d627b11c6fc016362
cataclym <ole.kh@hotmail.com>
2020-06-09 13:29:52 +0200

    Comments

--- commands/variables.js
+++ commands/variables.js
@@ -3,7 +3,7 @@ const prefixes = ["i'm ", "im ", "i am ", "i’m "];
 // Change these to words the bot should add reactions to //// Keep a space after words //// You can add as many as you want
 const prefixes2 = ["word1 ", "word2 "]; 
 const emotenames = ["emotename1", "emotename2", "emotename3", "emotename4"];
-//These are role names that the bot will exclude - Members having the roles wont trigger the bot.
+//These are role names that the bot will exclude - Members having the roles wont trigger the bot. //Currently not in use - Look in "../storage/names.json"
 const names = ["RoleName1", "RoleName2"];
 
 // Bot activity status
