5598c2ce57bcdd0828dfdf2d96bf1178b20bf3c8
Ole Hagberg <ole.kh@hotmail.com>
2020-06-12 13:06:54 +0200

    Tests

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 const fs = require('fs');
-const { prefixes, prefixes2, emotenames, activityname, activitystatus } = require("./commands/variables.js");
+const { prefixes2, emotenames, activityname, activitystatus } = require("./variables.js");
+//const functions = require('./functions/functions');
+const { rolecheck, handleMentions, dadbot } = require("./functions/functions");
 //Could go back to the names array for excluding multiple roles
 
+// Loads commands and sets their command execution
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 // boot
 client.once('ready', () => {
   console.log('Bot has finished booting sequence');
   client.user.setActivity(`${activityname}`, { type: `${activitystatus}` });
 });
 
-// handle commands
 client.on('message', message => {
+//These are the functions I want to load from "./functions/functions.js" Or alternatively their own files there...
+if (rolecheck(message)) // This is the check for excluded role.
+return;
 
-  const args = message.content.slice(config.prefix.length).split(' ');
-  const command = args.shift().toLowerCase();
-// Kills bot or restarts if running in pm2
-  if (command === 'die' && message.member.hasPermission('ADMINISTRATOR')) {
-    resetBot(message.channel);
-  }
+(handleMentions(message));
+(dadbot(message));
+
   for (const item of prefixes2)
   { //pulls the prefixes2 array
     const r = new RegExp("(^|\\s|$)(?<statement>(?<prefix>" + item + ")\\s*(?<nickname>.*)$)", "mi"); //regexp same as the one for dadbot command.
@@ -30,64 +38,20 @@ client.on('message', message => {
     message.react(emoji);
   }
 }
+// Handles commands that require the prefix from config
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-//btw this^^ could be prettier :))))) ^^
-  if (rolecheck(message)) // This is the check for excluded role.
-    return;
- 
-  handleMentions(message);
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-  dadbot(message);
+	if (!client.commands.has(command)) return;
 
-  if (!message.content.startsWith(config.prefix) || message.author.bot)
-    return;  
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply(`Error, ${error}`);
+	}
 });
-// handle mentions
-function handleMentions(message) {
-  if (message.mentions.has(client.user)) {
-    message.channel.startTyping(100)
-      .then(message.channel.send(`Hi ${message.author}, what is up?`))
-      .then(message.channel.stopTyping(true));
-  }
-};
-// dadbot
-function dadbot(message) {
-  for (const item of prefixes) {
-    const r = new RegExp("(^|\\s|$)(?<statement>(?<prefix>" + item + ")\\s*(?<nickname>.*)$)", "mi");
-    if (r.test(message.content) && !message.author.bot) {
-      const { nickname } = message.content.match(r).groups;
-      if (nickname.length <= 256) {
-        message.channel.send(`Hi, ${nickname}`);
-        const owner = message.guild.owner; 
-        if(nickname.length <= 32 && message.author.id !== owner.id) //Will ignore guild owner
-        message.member.setNickname(nickname).catch(error => {       //
-          if (error.code) {                                         // If any error it will log it in channel, console.
-            console.error('Failed to set nick due to:', error)      // Because owner is ignored already, it wont spam error in chat
-            message.channel.send(`Failed to set nick due to: ${error}`, error);
-          }
-        }
-        )
-      }
-      break;
-    }
-  }
-};
-// check for special role
-function rolecheck(message) {
-  const specialString = JSON.parse(fs.readFileSync("./storage/names.json", "utf8"));
-  if (message.member.roles.cache.find(r => r.name === specialString.name)) {
-    console.log("Role checked:", specialString.name);
-    return true;
-  }
-  return false;
-}
-// Turn bot off, then turn it back on.
-function resetBot(channel) {
-  // send channel a message that you're resetting bot.
-  channel.send('Shutting down :(')
-    .then(() => console.log('Shutting down'))
-    .then(() => process.exit(1));
 
-}
-
-client.login(config.token);
+client.login(token);
