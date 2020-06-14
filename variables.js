a93920aaf32f6301e57b032af13093966c1cd566
Ole Hagberg <ole.kh@hotmail.com>
2020-06-15 00:26:10 +0200

    no space after prefixes2

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 // Dont change
 const prefixes = ["i'm ", "im ", "i am ", "i’m "]; 
 // Change these to words the bot should add reactions to //// You can add as many as you want
-const prefixes2 = ["word1 ", "word2 "]; 
+const prefixes2 = ["word1", "word2"]; 
 const emotenames = ["emotename1", "emotename2", "emotename3", "emotename4"];
 
 //These are role names that the bot will exclude - Members having the roles wont trigger the bot. //Currently not in use go to (../storage/names.json)
