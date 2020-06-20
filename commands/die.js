a132e04a37fc093591013cb35cffb7c9576fe55f
Ole Hagberg <ole.kh@hotmail.com>
2020-06-20 08:31:32 +0200

    Pretty embeds

--- commands/die.js
+++ commands/die.js
@@ -1,10 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: "die",
     description: "Turn bot off, then turn it back on.",
     execute(message) {
+      let color = message.member.displayColor 
+      const embed = new MessageEmbed({
+          "title": "Shutting down now! 😦",
+          "color": color
+          })
     if (message.member.hasPermission('ADMINISTRATOR')) {
     // send channel a message that you're resetting bot.
-    message.channel.send('Shutting down :(')
+    message.channel.send(embed)
       .then(() => console.log('Shutting down'))
       .then(() => process.exit(1));
     }
--- commands/exclude.js
+++ commands/exclude.js
@@ -1,31 +1,47 @@
+const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: 'exclude',
     description: 'Adds or removes excluded role from user.',
     async execute(message) {   
         const specialString = require("../storage/names.json");
         const excludedRole = specialString.name
+        let color = message.member.displayColor
+// EMBEDS
+    const embed1 = new MessageEmbed({
+    "title": "Error!",
+    "description": `\`${excludedRole}\` was not found in guild. Creating... `,
+    "color": color,
+    "footer": {
+    "text": "Beep boop."}})
+    const embed2 = new MessageEmbed({
+    "title": "Success!",
+    "description": `Added role\`${excludedRole}\`.\nType the command again to remove.`,
+    "color": color})
+    const embed3 = new MessageEmbed({
+    "title": "Success!",
+    "description": `Removed role \`${excludedRole}\`.\nType the command again to add it back.`,
+    "color": color})
+// END
         if(!message.guild.roles.cache.find(r => r.name === specialString.name)) {
             message.guild.roles.create({
                 data: {
-                  name: specialString.name,
-                },
-                reason: "Role didn't exist yet",
-            })
-            .then(message.channel.send(`\`${excludedRole}\` was not found in guild. Creating... Beep boop.`))
+                name: specialString.name,},
+                reason: "Role didn't exist yet",})
+            .then(message.channel.send(embed1))
             .catch(console.error)
             setTimeout(() => {  (message.member.roles.add(message.guild.roles.cache.find(r => r.name === specialString.name))); }, 2000);
-            setTimeout(() => {  (message.channel.send(`Added role\`${excludedRole}\`.\nType the command again to remove.`)); }, 2000);
-            }
+            setTimeout(() => {  (message.channel.send(embed2)); }, 2000);
+}
             else if (!message.member.roles.cache.find(r => r.name === specialString.name)) {
             const excludedRole = specialString.name
             message.member.roles.add(message.guild.roles.cache.find(r => r.name === specialString.name))
-            message.channel.send(`Added role\`${excludedRole}\`.\nType the command again to remove.`);
-        }
+            message.channel.send(embed2);
+}
             else if (message.member.roles.cache.find(r => r.name === specialString.name)) {
             const excludedRole = specialString.name
             message.member.roles.remove(message.guild.roles.cache.find(r => r.name === specialString.name))
-            message.channel.send(`Removed role \`${excludedRole}\`.\nType the command again to add it back.`);
-            }
+            message.channel.send(embed3);
+}
     },
 };
 // We want to adapt it into the rewrite rjt did
