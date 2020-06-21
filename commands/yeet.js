bec1cd0444d2b01498d073384a5e63f1dfb035fc
Ole Hagberg <ole.kh@hotmail.com>
2020-06-21 22:56:48 +0200

    Update yeet.js

--- commands/yeet.js
+++ commands/yeet.js
@@ -3,42 +3,45 @@ const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
 module.exports = {
-    name: "yeet",
-    description: "",
-    args: false,
-    usage: `${prefix}yeet`,
-    execute(message) {
-    let color = message.member.displayColor
-    loadTitle(message)
-    message.channel.startTyping()
+  name: "yeet",
+  aliases: ['yeets'],
+  description: "Returns yeet...",
+  args: false,
+  usage: `${prefix}yeet`,
+  execute(message) {
+  let color = message.member.displayColor
+  loadTitle(message)
+  message.channel.startTyping()
 //Functions ->
-        function loadTitle() {
-            fetch('https://www.reddit.com/r/YEET.json?limit=1000&?sort=top&t=all')
-              .then(res => res.json())
-              .then(json => json.data.children.map(t => t.data))
-              .then(data => postRandomTitle(data))
-          }
-          function postRandomTitle(data) {
-            const randomTitle = data[Math.floor(Math.random() * data.length) + 1];
-            const RTSelftext = randomTitle.selftext.substring(0, 2048);
-            const RTTitle = randomTitle.title.substring(0, 256);
-            const embed = new Discord.MessageEmbed({
-                "title": RTTitle,
-                "description": RTSelftext,
-                "color": color,
-                "author": {
-                  "name": `Submitted by ${randomTitle.author}`
-                },
-                "image": {
-                    "url": `${randomTitle.url}`,
-                },
-                "footer": {
-                  "text": `${randomTitle.ups} updoots`
-                }
-              });
+function loadTitle() {
+fetch('https://www.reddit.com/r/DestructiveCriticism.json?limit=1000&?sort=top&t=all')
+  .then(res => res.json())
+  .then(json => json.data.children.map(t => t.data))
+  .then(data => postRandomTitle(data))
+}         
+function postRandomTitle(data) {
+const randomTitle = data[Math.floor(Math.random() * data.length) + 1];
+const RTSelftext = randomTitle.selftext.substring(0, 2045);
+if(randomTitle.selftext.length > 2048) {
+RTSelftext += "..."; }
+const RTTitle = randomTitle.title.substring(0, 256);
+const embed = new Discord.MessageEmbed({          
+  "title": RTTitle,
+  "description": RTSelftext,
+  "color": color,
+  "author": {
+    "name": `Submitted by ${randomTitle.author}`
+  },
+  "image": {
+      "url": `${randomTitle.url}`,
+  },
+  "footer": {
+    "text": `${randomTitle.ups} updoots`
+  }
+});
 //Functions <-
-    message.channel.stopTyping(true)
-    message.channel.send(embed);
-          } 
-    },
+  message.channel.stopTyping(true)
+  message.channel.send(embed);
+        } 
+  },
 }
