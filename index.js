f2613b722c4382c54702c9035c6aac44bb9eaae0
rjt <rjt.rockx@gmail.com>
2020-06-24 08:49:57 +0530

    Lint index.js too

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
-const fs = require('fs');
+const fs = require("fs");
 const { activityname, activitystatus } = require("./variables.js");
 const { emotereact, rolecheck, handleMentions, dadbot } = require("./functions/functions");
 //Could go back to the names array for excluding multiple roles
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 // boot
-client.once('ready', () => {
-	console.log('Bot has finished booting sequence');
+client.once("ready", () => {
+	console.log("Bot has finished booting sequence");
 	client.user.setActivity(`${activityname}`, { type: `${activitystatus}` });
 });
 
-client.on('message', message => {
+client.on("message", message => {
 
-	(handleMentions(message)); // Responds when pinged
-	(emotereact(message)); // Reacts to prefixes2 with emotenames
+	handleMentions(message); // Responds when pinged
+	emotereact(message); // Reacts to prefixes2 with emotenames
 	if (!rolecheck(message)) // This is the check for excluded role.
-		(dadbot(message)); // Handles "I am"
+		dadbot(message); // Handles "I am"
 
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
+	const args = message.content.slice(prefix.length).split(/\s+/);
 	const commandName = args.shift().toLowerCase();
 
 	//if (!client.commands.has(commandName)) return;
