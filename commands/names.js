e55c6b9feda87dd81c3594496b041d49cd786d5f
Ole Hagberg <ole.kh@hotmail.com>
2020-06-24 03:21:45 +0200

    Update names.js

--- commands/names.js
+++ commands/names.js
@@ -1,16 +1,15 @@
 const db = require('quick.db');
 const { UserNickTable } = require('../functions/functions.js');
 const { MessageEmbed } = require('discord.js');
+const { prefix } = require('../config.json');
 
 module.exports = {
     name: "names",
-    aliases: ['checknames','getnames'],
+    aliases: ['checknames','getnames','name'],
     description: "Returns all your daddy nicknames",
     //args: true,
     usage: '@someone',
     execute(message, args) {
-
-        let color = message.member.displayColor
         function getUserFromMention(mention) {
             if (!mention) return;
         
@@ -25,25 +24,47 @@ module.exports = {
         }
         const user = getUserFromMention(args[0]);
 
+        const AuthorDBName = UserNickTable.fetch(`usernicknames.${message.author.id}`);
+                // Makes it look cleaner
+        let StringsAuthorDBName = AuthorDBName.toString().substring(0, 2045);
+        StringsAuthorDBName = StringsAuthorDBName.replace(/,/g, ", ")
+        StringsAuthorDBName += "...";   
+
+        const av = message.mentions.users.first();
+        let color = message.member.displayColor
+
         if (args[0] && !user) {
             return message.reply('Please use a proper mention.');
-        }        
-             
-        const argsDBName = UserNickTable.fetch(`usernicknames.${args[0]}`);
-        const AuthorDBName = UserNickTable.fetch(`usernicknames.${message.member}`);
-        
+        }             
         const embed = new MessageEmbed()
-            .setTitle('Daddy will never forget')
-            .setDescription(`${args[0]}'s past names\n${argsDBName}`)
+            .setTitle(`${message.author.username}'s past names`)
+            .setAuthor('Daddy will never forget', 'https://cdn.discordapp.com/avatars/714695773534814238/c6b61ba085b7c1ff59716d1238860e0f.png',)
             .setColor(color)
-
+            .setDescription(`name here`)
+            .setFooter(`Delete these with ${prefix}delnames`)
+            .setTimestamp();
         if (!args[0]){
-            embed.setDescription(`${message.member}'s past names\n${AuthorDBName}`);
-        }    
-        /*if (argsDBName !== undefined || AuthorDBName !== undefined) {
-            embed.setDescription(`There is nothing here\nOutput: ${args[0]}, ${message.member}`)
-        }*/
-            
+            embed.setThumbnail(message.author.displayAvatarURL())
+            embed.setDescription(StringsAuthorDBName);
+        }   
+        if (args[0]){
+            const argsDBName = UserNickTable.fetch(`usernicknames.${user.id}`);
+            // Makes it look cleaner
+            let StringsargsDBName = argsDBName.toString().substring(0, 2045);
+            StringsargsDBName = StringsargsDBName.replace(/,/g, ", ")
+            StringsargsDBName += "..."
+            embed.setDescription(StringsargsDBName)
+            embed.setTitle(`${av.username}'s past names`)
+            embed.setThumbnail(av.displayAvatarURL());
+        }
+        const AuthorOrMention = args[0] || message.author;
+        if (embed.description.includes(undefined)) {
+            embed.setTitle('There is nothing here')
+            embed.setDescription(`${AuthorOrMention} may never have had their name changed by me`)
+            embed.setFooter('\u200B')
+            embed.setAuthor('Oops', 'https://cdn.discordapp.com/avatars/714695773534814238/c6b61ba085b7c1ff59716d1238860e0f.png');
+        }
+        
         return message.channel.send(embed);
         },
 }
