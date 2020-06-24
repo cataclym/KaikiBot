068d2c46efa38a69bf54fd3989ceec69d8dc3ce0
Ole Hagberg <ole.kh@hotmail.com>
2020-06-24 07:12:04 +0200

    indented++

--- commands/exclude.js
+++ commands/exclude.js
@@ -1,48 +1,48 @@
 /* eslint-disable max-len */
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: 'exclude',
-  description: 'Adds or removes excluded role from user.',
-  async execute(message) {
-    const specialString = require('../storage/names.json');
-    const excludedRole = specialString.name;
-    const color = message.member.displayColor;
-    // EMBEDS
-    const embed1 = new MessageEmbed({
-      title: 'Error!',
-      description: `\`${excludedRole}\` was not found in guild. Creating... `,
-      color,
-      footer: { text: 'Beep boop.' },
-    });
-    const embed2 = new MessageEmbed({
-      title: 'Success!',
-      description: `Added role \`${excludedRole}\`.\nType the command again to remove.`,
-      color,
-    });
-    const embed3 = new MessageEmbed({
-      title: 'Success!',
-      description: `Removed role \`${excludedRole}\`.\nType the command again to add it back.`,
-      color,
-    });
-    // END
-    if (!message.guild.roles.cache.find((r) => r.name === specialString.name)) {
-      message.guild.roles.create({
-        data: { name: specialString.name },
-        reason: "Role didn't exist yet",
-      })
-        .then(message.channel.send(embed1))
-        .catch(console.error);
-      setTimeout(() => { (message.member.roles.add(message.guild.roles.cache.find((r) => r.name === specialString.name))); }, 2000);
-      setTimeout(() => { (message.channel.send(embed2)); }, 2000);
-    } else if (!message.member.roles.cache.find((r) => r.name === specialString.name)) {
-      message.member.roles.add(message.guild.roles.cache.find((r) => r.name === specialString.name));
-      message.channel.send(embed2);
-    } else if (message.member.roles.cache.find((r) => r.name === specialString.name)) {
-      message.member.roles.remove(message.guild.roles.cache.find((r) => r.name === specialString.name));
-      message.channel.send(embed3);
-    }
-  },
+	name: "exclude",
+	description: "Adds or removes excluded role from user.",
+	async execute(message) {
+		const specialString = require("../storage/names.json");
+		const excludedRole = specialString.name;
+		const color = message.member.displayColor;
+		// EMBEDS
+		const embed1 = new MessageEmbed({
+			title: "Error!",
+			description: `\`${excludedRole}\` was not found in guild. Creating... `,
+			color,
+			footer: { text: "Beep boop." },
+		});
+		const embed2 = new MessageEmbed({
+			title: "Success!",
+			description: `Added role \`${excludedRole}\`.\nType the command again to remove.`,
+			color,
+		});
+		const embed3 = new MessageEmbed({
+			title: "Success!",
+			description: `Removed role \`${excludedRole}\`.\nType the command again to add it back.`,
+			color,
+		});
+		// END
+		if (!message.guild.roles.cache.find((r) => r.name === specialString.name)) {
+			message.guild.roles.create({
+				data: { name: specialString.name },
+				reason: "Role didn't exist yet",
+			})
+				.then(message.channel.send(embed1))
+				.catch(console.error);
+			setTimeout(() => { (message.member.roles.add(message.guild.roles.cache.find((r) => r.name === specialString.name))); }, 2000);
+			setTimeout(() => { (message.channel.send(embed2)); }, 2000);
+		} else if (!message.member.roles.cache.find((r) => r.name === specialString.name)) {
+			message.member.roles.add(message.guild.roles.cache.find((r) => r.name === specialString.name));
+			message.channel.send(embed2);
+		} else if (message.member.roles.cache.find((r) => r.name === specialString.name)) {
+			message.member.roles.remove(message.guild.roles.cache.find((r) => r.name === specialString.name));
+			message.channel.send(embed3);
+		}
+	},
 };
 // We want to adapt it into the rewrite rjt did
 // Not in use yet as it doesnt work perfectly
--- commands/test.js
+++ commands/test.js
@@ -1,35 +1,35 @@
-const { UserNickTable } = require('../functions/functions.js');
+const { UserNickTable } = require("../functions/functions.js");
 
 module.exports = {
-  name: 'test',
-  description: '',
-  // args: false,
-  // usage: ``,
-  execute(message, args) {
-    // eslint-disable-next-line no-unused-vars
-    const argsDBName = UserNickTable.fetch(`usernicknames.${args[0]}`);
-    const AuthorDBName = UserNickTable.fetch(`usernicknames.${message.member.id}`);
-    let strings = AuthorDBName.toString().substring(0, 2045);
-    strings = strings.replace(/,/g, ', ');
-    // eslint-disable-next-line no-unused-vars
-    strings += '...';
+	name: "test",
+	description: "",
+	// args: false,
+	// usage: ``,
+	execute(message, args) {
+		// eslint-disable-next-line no-unused-vars
+		const argsDBName = UserNickTable.fetch(`usernicknames.${args[0]}`);
+		const AuthorDBName = UserNickTable.fetch(`usernicknames.${message.member.id}`);
+		let strings = AuthorDBName.toString().substring(0, 2045);
+		strings = strings.replace(/,/g, ", ");
+		// eslint-disable-next-line no-unused-vars
+		strings += "...";
 
-    function getUserFromMention(mention) {
-      if (!mention) return;
+		function getUserFromMention(mention) {
+			if (!mention) return;
 
-      if (mention.startsWith('<@') && mention.endsWith('>')) {
-        mention = mention.slice(2, -1);
+			if (mention.startsWith("<@") && mention.endsWith(">")) {
+				mention = mention.slice(2, -1);
 
-        if (mention.startsWith('!')) {
-          mention = mention.slice(1);
-        }
-        return message.guild.members.cache.get(mention);
-      }
-    }
-    const user = getUserFromMention(args[0]);
-    const av = message.mentions.users.first();
+				if (mention.startsWith("!")) {
+					mention = mention.slice(1);
+				}
+				return message.guild.members.cache.get(mention);
+			}
+		}
+		const user = getUserFromMention(args[0]);
+		const av = message.mentions.users.first();
 
-    console.log(av.username, av.id);
-  }
-  ,
+		console.log(av.username, av.id);
+	}
+	,
 };
