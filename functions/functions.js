41b9486c0e5834783e76f4b96a06a5b9717ded65
Ole Hagberg <ole.kh@hotmail.com>
2020-07-20 23:24:10 +0200

    Added time left for rolls/likes

--- functions/functions.js
+++ functions/functions.js
@@ -148,6 +148,20 @@ function countEmotes(message) {
 	}
 }
 
+function msToTime(duration) {
+	var milliseconds = parseInt((duration % 1000) / 100),
+		seconds = Math.floor((duration / 1000) % 60),
+		minutes = Math.floor((duration / (1000 * 60)) % 60),
+		hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+	hours = (hours < 10) ? "0" + hours : hours;
+	minutes = (minutes < 10) ? "0" + minutes : minutes;
+	seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+	return hours + ":" + minutes + ":" + seconds + "." + milliseconds + " (hrs:min:sec.ms)";
+}
+
+
 module.exports = {
-	emotereact, rolecheck, handleMentions, dadbot, UserNickTable, TiredNadeko, getUserFromMention, ResetRolls, DailyResetTimer, EmoteDBStartup, countEmotes
+	emotereact, rolecheck, handleMentions, dadbot, UserNickTable, TiredNadeko, getUserFromMention, ResetRolls, DailyResetTimer, EmoteDBStartup, countEmotes, msToTime, timeToMidnight
 };
