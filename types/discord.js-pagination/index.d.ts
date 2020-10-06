00947205dd8169647e7f7c4251b3beacc318782c
Cata <ole.kh@hotmail.com>
2020-10-06 04:04:49 +0200

    Types for pagination

--- types/discord.js-pagination/index.d.ts
+++ types/discord.js-pagination/index.d.ts
@@ -1,7 +1,6 @@
-import { EmojiResolvable } from "discord.js";
-import { Message } from "discord.js";
-import { MessageEmbed } from "discord.js";
-
 declare module "discord.js-pagination" {
-    function paginationEmbed(msg: Message, pages: MessageEmbed[], emojilist: EmojiResolvable[], timeout: number): Message;  
+
+    import { EmojiResolvable, Message, MessageEmbed } from "discord.js";
+
+    export function paginationEmbed(msg: Message, pages: MessageEmbed[], emojilist?: EmojiResolvable[], timeout?: number): Message;  
 }
