38244da03f3bff03f95f36504fe9493d6d0cbad1
Cata <ole.kh@hotmail.com>
2020-10-07 02:26:39 +0200

    Don't need these anymore.

--- types/discord.js-pagination/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "discord.js-pagination" {
-
-    import { EmojiResolvable, Message, MessageEmbed } from "discord.js";
-
-    export function paginationEmbed(msg: Message, pages: MessageEmbed[], emojilist?: EmojiResolvable[], timeout?: number): Message;  
-}
