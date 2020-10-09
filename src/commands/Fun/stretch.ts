7639b44a1efcda5bb194bf4a09687fa5900ce96e
Cata <ole.kh@hotmail.com>
2020-10-09 14:11:13 +0200

    4 new commands, avatar+fun

---
+++ src/commands/Fun/stretch.ts
@@ -0,0 +1,34 @@
+import { Command } from "discord-akairo";
+import { GuildMember, Message, MessageEmbed, MessageAttachment } from "discord.js";
+import canvas from "canvas";
+
+export default class SquishCommand extends Command {
+	constructor() {
+		super("stretch", {
+			aliases: ["stretch"],
+			description: { description: "Stretches given member's avatar", usage: "@dreb" },
+			args: [
+				{
+					"id": "member",
+					"type": "member",
+					"default": (message: Message) => message.member,
+				},
+			],
+		});
+	}
+	public async exec(message: Message, { member }: { member: GuildMember}): Promise<Message> {
+		const picture = canvas.createCanvas(512, 128);
+		const squishImage = picture.getContext("2d");
+		const avatar = await canvas.loadImage(member.user.displayAvatarURL({ dynamic: true, size: 256, format: "png" }));
+		squishImage.drawImage(avatar, 0, 0, 512, 128);
+		const attachment: MessageAttachment = new MessageAttachment(picture.toBuffer(), "Stretched.jpg");
+		const embed = new MessageEmbed({
+			title: "Stretched avatar...",
+			image: { url: "attachment://Stretched.jpg" },
+			color: member.displayColor,
+		});
+
+
+		return message.channel.send({ files: [attachment], embed: embed });
+	}
+}
