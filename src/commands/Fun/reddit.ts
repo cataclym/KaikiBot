70f43eba6a38b2deb2ad40f155a0588fd9a6164a
Cata <ole.kh@hotmail.com>
2020-10-30 01:08:37 +0100

    Cant figure out the json resonses

--- src/commands/Fun/reddit.ts
+++ src/commands/Fun/reddit.ts
@@ -3,58 +3,55 @@ import { Command } from "discord-akairo";
 import Discord, { Message } from "discord.js";
 import { getMemberColorAsync } from "../../functions/Util";
 
-module.exports = class RedditCommand extends Command {
+export default class RedditCommand extends Command {
 	constructor() {
 		super("reddit", {
 			cooldown: 8000,
 			aliases: ["reddit"],
 			typing: true,
-			description: { description: "Returns a random reddit post, from a specified subreddit.", usage: "" },
+			description: { description: "Returns a random reddit post, from a specified subreddit.", usage: "anime" },
 			args: [
 				{
 					id: "sub",
 					match: "rest",
+					default: "anime",
 				},
 			],
 		});
 	}
 
-	async exec(message: Message, { sub }: { sub: string }) {
+	async exec(message: Message, { sub }: { sub: string }): Promise<Message | void> {
 
-		await loadTitle();
+		loadTitle();
+		const color = await getMemberColorAsync(message);
+
 		async function loadTitle() {
 			const promise = async () => fetch(`https://www.reddit.com/r/${sub}/random/.json?limit=1000&?sort=top&t=all`);
 			promise()
 				.then((res) => res.json())
-				.then((json) => json.data.children.map((t: any) => {
-					console.log(t);
-					console.log(typeof (t));
-					return t.data;
-				})
-					.then((data: any) => postRandomTitle(data)));
+				.then((json) => json[0].data.children.map((t: any) => t.data))
+				.then((data: any) => postRandomTitle(data));
 		}
 		async function postRandomTitle(data: any) {
+
+			data = data[0];
 
 			console.log(data);
 			console.log(typeof (data));
 
-			let randomTitleSelfText = data.selftext.substring(0, 2045);
-			if (data.selftext.length > 2048) {
-				randomTitleSelfText += "...";
-			}
-			const RTTitle = data.title.substring(0, 256);
 			const embed = new Discord.MessageEmbed({
-				title: RTTitle,
-				description: randomTitleSelfText,
-				color: await getMemberColorAsync(message),
+				color: color,
 				author: {
 					name: `Submitted by ${data.author}`,
 					url: data.url,
 				},
-				image: { url: `${data.url}` },
-				footer: { text: `${data.ups} updoots` },
+				footer: { text: `${data.ups} updoots / ${data.downs} downdoots` },
 			});
+			data.title?.length ? embed.setTitle(data.title.substring(0, 256)) : null;
+			data.selftext?.length ? embed.setDescription(data.selftext.substring(0, 2047)) : null;
+			!data.is_video ? embed.setImage(data.url) : message.channel.send(data.url);
+
 			return message.util?.send(embed);
 		}
 	}
-};
+}
