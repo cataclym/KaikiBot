ac224497a2ec81b76519eda27287bf155b94e33c
Cata <ole.kh@hotmail.com>
2020-11-19 15:24:12 +0100

    ✅ Added rolerename

--- src/commands/Roles/roleRename.ts
+++ src/commands/Roles/roleRename.ts
@@ -1,1 +1,64 @@
-// TODO
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed, Role, GuildMember } from "discord.js";
+import { noArgGeneric } from "../../functions/embeds";
+import { errorColor, getMemberColorAsync, trim } from "../../functions/Util";
+
+export default class RoleRenameCommand extends Command {
+	constructor() {
+		super("rolerename", {
+			aliases: ["rolerename", "rolename", "rn"],
+			description: { description: "Renames a given role. The role you specify has to be lower in the role hierarchy than your highest role. Use 'quotes around rolename with spaces'.", usage: "@Gamer weeb" },
+			clientPermissions: "MANAGE_ROLES",
+			userPermissions: "MANAGE_ROLES",
+			channel: "guild",
+			args: [
+				{
+					id: "role",
+					type: "role",
+					otherwise: () => new MessageEmbed({
+						title: "Can't find a matching role. Try again.",
+						color: errorColor,
+					}),
+				},
+				{
+					id: "name",
+					type: "string",
+					match: "rest",
+					otherwise: (msg: Message) => noArgGeneric(msg.util?.parsed?.command),
+				},
+			],
+		});
+	}
+	public async exec(message: Message, { role, name }: { role: Role, name: string }): Promise<Message> {
+
+		if ((role.position < (message.member as GuildMember).roles.highest.position) && !role.managed) {
+			try {
+				const editRole = role.edit({ name: trim(name.toString(), 32) });
+				if (editRole) {
+					return message.channel.send(new MessageEmbed({
+						title: "Success!",
+						description: `Role renamed to ${role}.`,
+						color: await getMemberColorAsync(message),
+					}));
+				}
+				else {
+					throw "Failed to edit role name.";
+				}
+			}
+			catch (e) {
+				return message.channel.send(new MessageEmbed({
+					title: "Error!",
+					description: "An error occured. Could not rename role.",
+					footer: { text: e },
+					color: errorColor,
+				}));
+			}
+		}
+		else {
+			return message.channel.send(new MessageEmbed({
+				title: "Insufficient permission(s).",
+				color: errorColor,
+			}));
+		}
+	}
+}
