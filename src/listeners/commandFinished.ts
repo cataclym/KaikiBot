b7dcdb3ea73c3cbc0641154e0d98f132c1cc752a
Cata <ole.kh@hotmail.com>
2020-12-04 23:29:48 +0100

    Edit: (untested) cmd logger also logs dm cmds

--- src/listeners/commandFinished.ts
+++ src/listeners/commandFinished.ts
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import { Command, Listener } from "discord-akairo";
 import { Message } from "discord.js";
 
@@ -10,15 +11,22 @@ export default class commandFinishedListener extends Listener {
 	}
 
 	public async exec(message: Message, command: Command): Promise<void> {
+		const date = new Date().toLocaleString("en-GB", { hour: "2-digit", minute: "2-digit", second: "2-digit", weekday: "short", year: "numeric", month: "numeric", day: "numeric" });
+
 		if (message.channel.type !== "dm") {
-			const date = new Date().toLocaleString("en-GB", { hour: "2-digit", minute: "2-digit", second: "2-digit", weekday: "short", year: "numeric", month: "numeric", day: "numeric" });
 			console.log(
-				// eslint-disable-next-line
 	`🟢	${date} CommandFinished | ${Date.now() - message.createdTimestamp}ms
 	Guild: ${message.guild?.name} [${message.guild?.id}]
 	Channel: #${message.channel.name} [${message.channel.id}]
 	User: ${message.author.username} [${message.author.id}]
-	Executed ${command.id} | "${message.content}"`);
+	Executed ${command?.id} | "${message.content}"`);
+		}
+		else {
+			console.log(
+	`🟢	${date} CommandFinished | ${Date.now() - message.createdTimestamp}ms
+	Channel: PRIVATE [${message.channel.id}]
+	User: ${message.author.username} [${message.author.id}]
+	Executed ${command?.id} | "${message.content}"`);
 		}
 	}
 }
