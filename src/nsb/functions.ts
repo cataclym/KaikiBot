8f1521ac46b51542e9a01ffd40790a3685142449
Cata <ole.kh@hotmail.com>
2021-02-19 01:13:21 +0100

    functions: db does count emotes

--- src/nsb/functions.ts
+++ src/nsb/functions.ts
@@ -1,8 +1,9 @@
-import { Collection, Message, Client, Guild } from "discord.js";
+import { Message, Guild } from "discord.js";
 import { getGuildDB } from "../struct/db";
 import { config } from "../config";
 import { logger } from "./Logger";
 import { tinderDataDB } from "../struct/models";
+import { AkairoClient } from "@cataclym/discord-akairo";
 
 const words = ["shit", "fuck", "stop", "dont", "kill", "don't", "don`t", "fucking", "shut", "shutup", "shuttup", "trash", "bad", "hate", "stupid", "dumb", "suck", "sucks"];
 const index: {[name: string]: number} = {
@@ -43,13 +44,12 @@ async function tiredNadekoReact(message: Message): Promise<void> {
 
 async function ResetRolls(): Promise<void> {
 	// Tinder reset
-	tinderDataDB.updateMany({ tinderData: { $type: "object" } }, { $set: { "tinderData.temporary": [], "tinderData.rolls": 15, "tinderData.likes": 3 } }, null, () => {
+	tinderDataDB.updateMany({ rolls: { $lt: 15 } }, { $set: { "tinderData.temporary": [], "tinderData.rolls": 15, "tinderData.likes": 3 } }, null, () => {
 		logger.info(`mongooseDB | Reset tinder rolls/likes at ${Date()}`);
 	});
 }
 
 async function dailyResetTimer(): Promise<void> {
-	ResetRolls();
 	setTimeout(async () => {
 		ResetRolls();
 		dailyResetTimer();
@@ -61,50 +61,58 @@ function timeToMidnight(): number {
 	return (-d + d.setHours(24, 0, 0, 0));
 }
 
-async function emoteDB(guild: Guild, i = 0) {
-	const gDB = await getGuildDB(guild.id);
-	guild.emojis.cache.forEach(async emote => {
-
-		if (!gDB.emojiStats[emote.id]) {
-			gDB.emojiStats[emote.id] = 0;
+async function emoteDB(guild: Guild) {
+	// eslint-disable-next-line no-var
+	var i = 0;
+	const db = await getGuildDB(guild.id);
+	await guild.emojis.cache.reduce(async (promise, emote) => {
+		// This line will wait for the last async function to finish.
+		// The first iteration uses an already resolved Promise
+		// so, it will immediately continue.
+		await promise;
+		if (!(emote.id in db.emojiStats)) {
 			i++;
+			db.emojiStats[emote.id] = 0;
 		}
-	});
-	gDB.save();
-	return Promise.resolve(i);
+	}, Promise.resolve());
+	db.markModified("emojistats");
+	await db.save();
+	return i;
 }
 
-async function emoteDataBaseService(input: Client | Guild): Promise<number> {
-
-	if (input instanceof Client) {
-		let i = 0;
-		input.guilds.cache.forEach(async guild => {
-			emoteDB(guild);
-			i++;
-		});
-		return Promise.resolve(i);
+async function emoteDataBaseService(input: AkairoClient | Guild): Promise<number> {
+	// eslint-disable-next-line no-var
+	var changes = 0;
+	if (input instanceof AkairoClient) {
+		await input.guilds.cache.reduce(async (promise, guild) => {
+			await promise;
+			changes = await emoteDB(guild) + changes;
+		}, Promise.resolve());
+		return changes;
 	}
-
 	else {
-		return emoteDB(input);
+		return await emoteDB(input);
 	}
 }
 
 async function countEmotes(message: Message): Promise<void> {
-	const emotes = message.content.match(/<?(a)?:.+?:\d+>/g),
-		{ guild } = message;
-	if (emotes && guild) {
-		const ids = emotes.toString().match(/\d+/g);
-		ids?.forEach(async id => {
-			const emote = guild.emojis.cache.find(emoji => emoji.id === id);
-			if (emote) {
-				const db = await getGuildDB(guild.id);
-				db.emojiStats[emote.id]
-					? db.emojiStats[emote.id]++
-					: db.emojiStats[emote.id] = 1;
-				db.save();
-			}
-		});
+	if (message.guild) {
+		const { guild } = message,
+			emotes = message.content.match(/<?(a)?:.+?:\d+>/g);
+		if (emotes && guild) {
+			const ids = emotes.toString().match(/\d+/g);
+			ids?.forEach(async id => {
+				const emote = guild.emojis.cache.find(emoji => emoji.id === id);
+				if (emote) {
+					const db = await getGuildDB(guild.id);
+					db.emojiStats[emote.id]
+						? db.emojiStats[emote.id]++
+						: db.emojiStats[emote.id] = 1;
+					db.markModified("emojiStats");
+					db.save();
+				}
+			});
+		}
 	}
 }
 
@@ -121,17 +129,9 @@ function msToTime(duration: number): string {
 	return "**" + hours + "** hours **" + minutes + "** minutes **" + seconds + "." + milliseconds + "** seconds";
 }
 
-async function dbColumns(client: Client): Promise<Collection<string, Guild>> {
-	const guilds = client.guilds.cache.each(g => {
-		return getGuildDB(g.id);
-	});
-	return Promise.resolve(guilds);
-}
-
 export {
 	countEmotes,
 	dailyResetTimer,
-	dbColumns,
 	emoteDataBaseService,
 	emoteReact,
 	msToTime,
