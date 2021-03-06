591da7254dd00d994451030a901e3505ef136cee
Cata <ole.kh@hotmail.com>
2021-03-06 01:37:08 +0100

    fix merge logger

--- src/commands/Owner only/update.ts
+++ src/commands/Owner only/update.ts
@@ -1,9 +1,8 @@
 import { Command } from "@cataclym/discord-akairo";
 import { exec } from "child_process";
 import { Message } from "discord.js";
+import logger from "loglevel";
 import { codeblock } from "../../nsb/Util";
-import { logger } from "../../nsb/Logger";
-
 export default class UpdateCommand extends Command {
 	constructor() {
 		super("update", {
@@ -14,12 +13,12 @@ export default class UpdateCommand extends Command {
 	public async exec(message: Message): Promise<void> {
 		exec("git pull", async (err, std) => {
 			if (err) {
-				logger.high(err);
+				logger.error(err);
 				return message.channel.send(err.message);
 			}
 			exec("git describe --tags", async (error, stdVer) => {
 				if (error) {
-					logger.high(error);
+					logger.error(error);
 					return message.channel.send(error.message);
 				}
 				return message.channel.send(`Log:\n${await codeblock(std, undefined)}\nUpdated ${message.client.user?.tag} to ${stdVer}`);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { extensionHook } from "./Extensions/Discord";
-import logger from "loglevel";
-import { customClient } from "./struct/client";
-import { config } from "./config";
 import { exec } from "child_process";
+import logger from "loglevel";
+import { config } from "./config";
+import { extensionHook } from "./Extensions/Discord";
+import { customClient } from "./struct/client";
 
 logger.setLevel("INFO", true);
 // Shmart
 exec("git update-index --assume-unchanged src/config.ts", (err, stdout, stderr) => {
-	if (err) logger.high("Untracking changes to config file\n" + err);
-	if (stderr) logger.high("Untracking changes to config file\n" + stderr);
+	if (err) logger.error("Untracking changes to config file\n" + err);
+	if (stderr) logger.error("Untracking changes to config file\n" + stderr);
 	logger.info("Untracking changes to src/config.ts");
 });
 
