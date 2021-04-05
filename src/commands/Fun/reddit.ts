358cc84ccf8057373247b1b691bf86162c8bf4bc
Cata <11278743+cataclym@users.noreply.github.com>
2021-04-05 22:41:21 +0200

    Reddit Interface/model moved

--- src/commands/Fun/reddit.ts
+++ src/commands/Fun/reddit.ts
@@ -1,13 +1,12 @@
-import fetch from "node-fetch";
 import { Command } from "@cataclym/discord-akairo";
 import { Message, MessageEmbed, TextChannel } from "discord.js";
+import fetch from "node-fetch";
+import { PurpleData, RedditData } from "../../interfaces/IRedditAPI";
 import { trim } from "../../nsb/Util";
-import { redditData, Data1 } from "../../struct/redditModel";
 
 export default class RedditCommand extends Command {
 	constructor() {
 		super("reddit", {
-			cooldown: 6000,
 			aliases: ["reddit"],
 			typing: true,
 			description: { description: "Returns a random reddit post, from a specified subreddit.", usage: "anime" },
@@ -23,23 +22,22 @@ export default class RedditCommand extends Command {
 
 	public async exec(message: Message, { sub }: { sub: string }): Promise<Message> {
 
-		return (async function loadTitle() {
-			const file: redditData = await fetch(`https://www.reddit.com/r/${sub}/random/.json`)
-				.then(response => response.json());
-			if (file?.data?.children?.length) {
-				const data = file.data.children.map(a => a.data);
-				return postRandomTitle(data[Math.floor(Math.random() * data.length)]);
-			}
-			else {
+		const promise = async () => fetch(`https://www.reddit.com/r/${sub.trim()}/random/.json`);
+
+		return await promise()
+			.then(response => response.json())
+			.then((json: RedditData | RedditData[]) => Array.isArray(json) ? json[0].data.children.map((t) => t.data) : json.data.children.map((t) => t.data))
+			.then((data) => postRandomTitle(data[Math.floor(Math.random() * data.length)]));
+
+		async function postRandomTitle(data: PurpleData) {
+
+			if (!data) {
 				return message.channel.send(new MessageEmbed({
 					title: "Error",
 					description: "No data received...",
 				})
 					.withErrorColor(message));
 			}
-		})();
-
-		async function postRandomTitle(data: Data1) {
 
 			// We don´t want nsfw in normal channels, do we?
 			if (data.over_18 && (!(message.channel as TextChannel)?.nsfw ||	message.channel.type !== "dm")) {
@@ -62,7 +60,7 @@ export default class RedditCommand extends Command {
 
 			if (data.title?.length) embed.setTitle(trim(data.title, 256));
 			if (data.selftext?.length) embed.setDescription(trim(data.selftext, 2048));
-			!data.is_video ? embed.setImage(data.url) : message.channel.send(data.url);
+			!data.is_video && data.url?.length ? embed.setImage(data.url) : message.channel.send(data.url ?? data.permalink);
 
 			return message.channel.send(embed);
 		}
