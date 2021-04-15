ffc060ef718adc78d2ef61800bc2a43edafa26b2
Cata <11278743+cataclym@users.noreply.github.com>
2021-04-16 01:09:51 +0200

    new reaction, api endpoint

---
+++ src/commands/Reactions/spank.ts
@@ -0,0 +1,32 @@
+import { Command } from "@cataclym/discord-akairo";
+import { GuildMember, Message, MessageEmbed } from "discord.js";
+import sendNekosPics from "../../nsb/nekos.life";
+
+export default class spank extends Command {
+	constructor() {
+		super("spank", {
+			aliases: ["spank"],
+			description: { description: "OwO Being naughty are we?",
+				usage: ["", "@dreb"] },
+			typing: true,
+			args: [{
+				id: "mention",
+				type: "member",
+				default: null,
+			}],
+		});
+	}
+	public async exec(message: Message, { mention }: { mention: GuildMember | null }): Promise<Message> {
+		if (message.channel.type === "text" && message.channel.nsfw) {
+			return message.channel.send(await sendNekosPics(message, "spank", mention));
+		}
+
+		else {
+			return message.channel.send(new MessageEmbed({
+				title: "Error",
+				description: "Channel is not NSFW.",
+			})
+				.withErrorColor(message));
+		}
+	}
+}
---
+++ src/nsb/nekos.life.ts
@@ -0,0 +1,34 @@
+import { ColorResolvable, GuildMember, Message, MessageEmbed } from "discord.js";
+import fetch from "node-fetch";
+import { hexColorTable } from "./Color";
+
+type APIs = "spank";
+
+const NekosData: {
+        [str in APIs]: { action: string | boolean, color: ColorResolvable, append?: string }
+	} = {
+		"spank": {
+			action: "spanked",
+			color: hexColorTable["peachpuff"],
+			append: "🍑👋",
+		},
+	};
+
+export default async function sendNekosPics(message: Message, API: APIs, mention?: GuildMember | null): Promise<MessageEmbed> {
+
+	const data = NekosData[API],
+		{ action, color, append } = data,
+		result = await (await (await fetch(`https://nekos.life/api/v2/img/${API}`)).json())["url"],
+		embed = new MessageEmbed({
+			color: color,
+			image: { url: result },
+			footer: { icon_url: (mention?.user || message.author).displayAvatarURL({ dynamic: true }) },
+		});
+
+	if (mention && action) {
+		embed.setDescription(`${message.author.username} ${action} ${mention.user.username} ${append ?? ""}`);
+	}
+
+	return embed;
+}
+
