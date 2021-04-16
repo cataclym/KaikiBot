bbdc5f9517b36c50c62d4bf3d1a98660e174e6ad
Cata <11278743+cataclym@users.noreply.github.com>
2021-04-16 23:25:53 +0200

    Possible workaround for ts compiler to import package.json

--- src/commands/Utility/help.ts
+++ src/commands/Utility/help.ts
@@ -1,5 +1,7 @@
 import { Argument, Command, PrefixSupplier } from "@cataclym/discord-akairo";
+import { execSync } from "child_process";
 import { Message, MessageEmbed } from "discord.js";
+import { name, version } from "../../../package.json";
 
 export default class HelpCommand extends Command {
 	constructor() {
@@ -26,13 +28,9 @@ export default class HelpCommand extends Command {
 			let usage = command.description.usage;
 
 			if (usage) {
-				if (usage instanceof Array) {
-					usage = usage.map(u => `${prefix}${command.id} ${u}`).join("\n");
-				}
-				else {
-					usage = `${prefix}${command.id} ${usage}`;
-				}
+				usage = usage instanceof Array ? usage.map(u => `${prefix}${command.id} ${u}`).join("\n") : `${prefix}${command.id} ${usage}`;
 			}
+
 			embed.setTitle(`**Name:** ${command.id}`);
 			embed.setDescription(`**Aliases:** \`${command.aliases.join("`, `")}\`\n**Description:** ${command.description.description || command.description}\n`);
 
@@ -42,6 +40,7 @@ export default class HelpCommand extends Command {
 
 			return message.channel.send(embed);
 		}
+
 		else if (typeof command === "string") {
 			return message.channel.send(new MessageEmbed({
 				description: `**${message.author.tag}** Command \`${command}\` not found.`,
@@ -49,16 +48,19 @@ export default class HelpCommand extends Command {
 				.withErrorColor(message));
 		}
 
-		const AvUrl = await message.client.users.fetch("140788173885276160", true);
-		// Bot author
+		const AvUrl = (message.client.users.cache.get("140788173885276160") || (await message.client.users.fetch("140788173885276160", true)))
+			.displayAvatarURL({ dynamic: true });
+
 		embed.setTitle(`${message.client.user?.username} help page`);
 		embed.setDescription(`Prefix is currently set to \`${prefix}\``);
 		embed.addFields([
 			{ name: "📋 Command list", value: `\`${prefix}cmds\` returns a complete list of commands.`, inline: true },
 			{ name: "🔍 Command Info", value: `\`${prefix}help [command]\` to get more help. Example: \`${prefix}help ping\``, inline: true },
 		]);
-		embed.setAuthor(`Nadeko Sengoku Bot v${process.env.npm_package_version}`, message.author.displayAvatarURL({ dynamic: true }), "https://gitlab.com/cataclym/nadekosengokubot");
-		embed.setFooter("Made by Cata <3", AvUrl.displayAvatarURL({ dynamic: true }));
+		embed.setAuthor(`${name} v${version}-${execSync("git rev-parse --short HEAD").toString()}`,
+			message.author.displayAvatarURL({ dynamic: true }), "https://gitlab.com/cataclym/nadekosengokubot");
+		embed.setFooter("Made by Cata <3", AvUrl);
 		return message.channel.send(embed);
 	}
 }
+
