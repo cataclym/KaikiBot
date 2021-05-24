0e1b38aabfb7966ece7048a1b642a3cc703aa07c
Cata <11278743+cataclym@users.noreply.github.com>
2021-05-25 00:17:10 +0200

    Added: hentainuke command 🤧

---
+++ src/commands/NSFW/hentainuke.ts
@@ -0,0 +1,40 @@
+import { Command } from "@cataclym/discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+import { grabHentai, typesArray } from "./hentaiService";
+
+export default class HentaiBombCommand extends Command {
+	constructor() {
+		super("hentainuke", {
+			aliases: ["hentainuke", "hn"],
+			description: { description: "Posts 30 NSFW images, using the waifu.pics API",
+				usage: typesArray },
+			args: [
+				{
+					id: "category",
+					type: typesArray,
+					default: null,
+				},
+			],
+		});
+	}
+
+	public async exec(message: Message, { category }: { category: "waifu" | "neko" | "trap" | "blowjob" | null }): Promise<void | Message> {
+
+		if (message.channel.type === "text" && message.channel.nsfw) {
+
+			const megaResponse = (await grabHentai(category ?? typesArray[Math.floor(Math.random() * typesArray.length)], "bomb"));
+
+			for (let index = 5, p = 0; p < megaResponse.length; index += 5, p += 5) {
+				await message.channel.send(megaResponse.slice(p, index));
+			}
+		}
+
+		else {
+			return message.channel.send(new MessageEmbed({
+				title: "Error",
+				description: "Channel is not NSFW.",
+			})
+				.withErrorColor(message));
+		}
+	}
+}
