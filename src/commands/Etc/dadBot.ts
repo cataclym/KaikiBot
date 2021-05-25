9cedfa60568a771fcf1d216b48776dd3bd669cbc
Cata <11278743+cataclym@users.noreply.github.com>
2021-05-26 00:08:15 +0200

    daddy pls

--- src/commands/Etc/dadBot.ts
+++ src/commands/Etc/dadBot.ts
@@ -1,12 +1,11 @@
 import { Command } from "@cataclym/discord-akairo";
-import { Guild, GuildMember, Message, Util } from "discord.js";
-import { IUser } from "../../interfaces/db";
+import { Message, Util } from "discord.js";
 import { dadbotArray } from "../../struct/constants";
 import { getUserDB } from "../../struct/db";
 
-let nick: {
-	[key: string]: string
-};
+const nickname: {
+	[id: string]: string
+} = {};
 
 // dad bot
 export default class dadBot extends Command {
@@ -15,48 +14,55 @@ export default class dadBot extends Command {
 			channel: "guild",
 			editable: false,
 			condition: (message: Message) => {
+				if (!message.guild || !message.member || message.author.bot) return false;
 
-				if (!message.guild) return false;
+				if (message.guild.isDadBotEnabled() && message.member.hasExcludedRole() && !message.content.includes("||")) {
 
-				if ((message.guild as Guild).isDadBotEnabled() && (message.member as GuildMember).hasExcludedRole() && !message.author.bot) {
 					for (const item of dadbotArray) {
 
 						const r = new RegExp(`(^|\\s|$)(?<statement>(?<prefix>${item})\\s*(?<nickname>.*)$)`, "mi");
 
-						if (r.test(message.content) && !message.content.includes("||")) {
+						if (r.test(message.content)) {
 
-							const match = message.content.match(r)?.groups;
+							let match = message.content.match(r)?.groups?.nickname;
 
-							if (match?.nickname && match?.nickname.length <= 256) {
-								nick = match;
-								return true;
+							if (!match) continue;
+
+							const splits = match.split(new RegExp(`${item}`, "mig"));
+
+							if (splits.length > 1) match = splits.reduce((a, b) => a.length <= b.length ? a : b);
+
+							if (match.length <= 256) {
+								if (!nickname[message.member.id] || nickname[message.member.id]?.length > match.length) nickname[message.member.id] = match;
 							}
-							return false;
 						}
 					}
+					return !!(nickname[message.member.id]);
 				}
 				return false;
 			},
 		});
 	}
 
-	public async exec(message: Message): Promise<IUser | void> {
+	public async exec(message: Message): Promise<boolean> {
 
-		message.channel.send(`Hi, ${Util.removeMentions(nick.nickname)}`);
+		const nick = nickname[message.member!.id];
 
-		if (nick.nickname.length <= 32) {
+		message.channel.send(`Hi, ${Util.removeMentions(nick)}`);
 
+		if (nick.length <= 32) {
 			const user = message.author,
 				db = await getUserDB(user.id);
 
-			db.userNicknames.push(nick.nickname);
+			db.userNicknames.push(nick);
 
 			if (user.id !== message.guild?.ownerID) {
 				// Avoids setting nickname on Server owners
-				message.member?.setNickname(nick.nickname);
+				message.member?.setNickname(nick);
 			}
 			db.markModified("userNicknames");
-			return db.save();
+			db.save();
 		}
+		return delete nickname[message.member!.id];
 	}
 }
