e488fed5cb46674cd223a1bf0adf6568f936fc87
Cata <11278743+cataclym@users.noreply.github.com>
2021-06-02 22:16:01 +0200

    Injectable types with inversifyJS

---
+++ src/struct/bot.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "inversify";
+import { customClient } from "./client";
+import { TYPES } from "./types";
+
+@injectable()
+export class Bot {
+    private client: customClient;
+    private readonly token: string;
+
+    constructor(
+        @inject(TYPES.Client) client: customClient,
+        @inject(TYPES.Token) token: string,
+    ) {
+
+    	this.client = client;
+    	this.token = token;
+    }
+
+    public start(): Promise <string> {
+
+    	if (!process.env.PREFIX) {
+    		throw new Error("Missing prefix! Set a prefix in .env");
+    	}
+
+    	if (!process.env.OWNER) {
+    		throw new Error("Missing owner-ID! Please double-check the guide and set an owner in .env");
+    	}
+
+    	return this.client.login(this.token);
+    }
+}
---
+++ src/struct/types.ts
@@ -0,0 +1,5 @@
+export const TYPES = {
+	Bot: Symbol("Bot"),
+	Client: Symbol("Client"),
+	Token: Symbol("Token"),
+};
