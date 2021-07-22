804b31bbdd598be7494ce6bed59fe302b0953176
Cata <11278743+cataclym@users.noreply.github.com>
2021-07-22 21:55:22 +0200

    Re-added shardstats. Not tested with more than one shard.

---
+++ src/commands/Utility/shardstats.ts
@@ -0,0 +1,32 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { codeblock } from "../../lib/Util";
+
+const shardStats: {[index: number]: string} = {
+	0: "READY",
+	1: "CONNECTING",
+	2: "RECONNECTING",
+	3: "IDLE",
+	4: "NEARLY",
+	5: "DISCONNECTED",
+};
+
+module.exports = class ShardStatisticsCommand extends Command {
+	constructor() {
+		super("shardstats", {
+			aliases: ["shards", "shardstats"],
+			description: "Shows state of shards",
+			channel: "guild",
+		});
+	}
+	public async exec(message: Message) {
+
+		const { ws } = message.client;
+
+		return message.channel.send({ content: await codeblock(`This guild is managed by shard: [${message.guild!.shardID}]`, "xl") +
+			await codeblock(Array.from(ws.shards.entries())
+				.map(([_, w]) => `ID: [${w.id}] | Ping: ${w.ping}ms | Status: ${shardStats[w.status]}`)
+				.join("\n"), "xl"),
+		});
+	}
+};
