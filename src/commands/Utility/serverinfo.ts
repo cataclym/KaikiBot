5ceb19363e56f4a3247ad8c9e39cc9c69c1dee8a
Cata <11278743+cataclym@users.noreply.github.com>
2021-12-19 23:18:08 +0100

    very minor changes

--- src/commands/Utility/serverinfo.ts
+++ src/commands/Utility/serverinfo.ts
@@ -1,4 +1,3 @@
-import { PrefixSupplier } from "discord-akairo";
 import { Guild, Message, MessageEmbed } from "discord.js";
 import { KaikiCommand } from "kaiki";
 import { guildFeatures } from "../../struct/constants";
@@ -27,21 +26,19 @@ export default class ServerInfoCommand extends KaikiCommand {
 			fields: [
 				{ name: "Owner", value: message.client.users.cache.get(guild.ownerId)?.tag ?? guild.ownerId, inline: true },
 				{ name: "Members", value: String(guild?.memberCount), inline: true },
-				{ name:
-                    "Channels", value: "Text: **" + guild?.channels.cache.filter((channel) => channel.type === "GUILD_TEXT").size +
-                    "**\nVoice: **" + guild?.channels.cache.filter((channel) => channel.type === "GUILD_VOICE").size +
-                    "**\nCategories: **" + guild?.channels.cache.filter((channel) => channel.type === "GUILD_CATEGORY").size +
-                    "**\nNews: **" + guild?.channels.cache.filter((channel) => channel.type === "GUILD_NEWS").size +
-                    "**\nStore: **" + guild?.channels.cache.filter((channel) => channel.type === "GUILD_STORE").size + "**", inline: true },
+				{ name: "Channels", value: `Text: **${guild?.channels.cache.filter((channel) => channel.type === "GUILD_TEXT").size}**
+Voice: **${guild?.channels.cache.filter((channel) => channel.type === "GUILD_VOICE").size}**
+Categories: **${guild?.channels.cache.filter((channel) => channel.type === "GUILD_CATEGORY").size}**
+News: **${guild?.channels.cache.filter((channel) => channel.type === "GUILD_NEWS").size}**
+Store: **${guild?.channels.cache.filter((channel) => channel.type === "GUILD_STORE").size}**`, inline: true },
 				{ name: "Created At", value: time(guild?.createdAt), inline: true },
 				{ name: "Roles", value: String(guild?.roles.cache.size), inline: true },
-				{ name: "Emotes", value: "Count: **" + guild?.emojis.cache.size +
-                    "**\nSee them with `" + (this.handler.prefix as PrefixSupplier)(message) + "emotecount`", inline: true },
+				{ name: "Emotes", value: `Count: **${guild?.emojis.cache.size}**`, inline: true },
+				{ name: "Features", value: guild?.features.length
+					? guild?.features.map(f => guildFeatures[f] || f).sort().join("\n")
+					: "None", inline: false },
 			],
-		})
-			.addField("Features", guild?.features.length
-				? guild?.features.map(f => guildFeatures[f] || f).sort().join("\n")
-				: "None", false);
+		});
 
 		return message.channel.send({ embeds: [emb.withOkColor(message)] });
 	}
