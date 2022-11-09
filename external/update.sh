7f294373ccabd44c7e776b043b63115be708921a
cataclym <ole.kh@hotmail.com>
2022-11-09 23:21:56 +0100

    5.0.0-5

--- external/update.sh
+++ external/update.sh
@@ -2,17 +2,16 @@
 echo "This is a placeholder. Updates will be implemented in v4.1+"
 currentTag=$(git describe)
 
-git fetch --tags
-
 latestTag=$(git tag | tail -1 )
 
-if ["$latestTag" != "$currentTag"]; then
+if [ "$latestTag" != "$currentTag" ]; then
   echo "New version detected. Updating from ${currentTag} to ${latestTag}..."
   git checkout "$latestTag"
-  if ["$latestTag" == "$currentTag"]; then
+  if [ "$latestTag" == "$currentTag" ]; then
     echo "Successfully updated to ${latestTag}!"
   else
     echo "Error, unable to update..."
+  fi
 fi
 
 echo "No update available."
