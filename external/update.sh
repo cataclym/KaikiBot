97c04916557466ce84b8fd96f5b832559ade01fc
Cata <11278743+cataclym@users.noreply.github.com>
2022-11-09 23:21:35 +0100

    Testing update script

--- external/update.sh
+++ external/update.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/bash
 echo "This is a placeholder. Updates will be implemented in v4.1+"
-#currentTag =
-#
-#git fetch --tags
-#
-#latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-#
-#if ["$latestTag" == ]
-#git checkout "$latestTag" package-lock.json
+currentTag=$(git describe)
+
+git fetch --tags
+
+latestTag=$(git tag | tail -1 )
+
+if ["$latestTag" != "$currentTag"]; then
+  echo "New version detected. Updating from ${currentTag} to ${latestTag}..."
+  git checkout "$latestTag"
+  if ["$latestTag" == "$currentTag"]; then
+    echo "Successfully updated to ${latestTag}!"
+  else
+    echo "Error, unable to update..."
+fi
+
+echo "No update available."
