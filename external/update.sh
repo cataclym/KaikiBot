89a86e90e2b2872346fc6910ac9d39f24021afbb
Cata <11278743+cataclym@users.noreply.github.com>
2022-11-10 02:52:50 +0100

    Update script (probably) works

--- external/update.sh
+++ external/update.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/bash
-echo "This is a placeholder. Updates will be implemented in v4.1+"
 currentTag=$(git describe)
-
 latestTag=$(git tag | tail -1 )
 
-if [ "$latestTag" != "$currentTag" ]; then
+if [[ "$latestTag" != "$currentTag" ]]; then
   echo "New version detected. Updating from ${currentTag} to ${latestTag}..."
-  git checkout "$latestTag"
-  if [ "$latestTag" == "$currentTag" ]; then
+  git checkout "$latestTag" && currentTag=$(git describe)
+
+  if [[ "$latestTag" == "$currentTag" ]]; then
     echo "Successfully updated to ${latestTag}!"
+    exit 0
   else
     echo "Error, unable to update..."
+    exit 1
   fi
 fi
 
 echo "No update available."
+exit 0
