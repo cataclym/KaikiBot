6d1e22de1e4728db3c15c7c44b0fa91653191908
Cata <11278743+cataclym@users.noreply.github.com>
2022-11-14 19:05:05 +0100

    Added: Update cmd. (Needs to be tested)

--- src/commands/Owner only/update.ts
+++ src/commands/Owner only/update.ts
@@ -1,15 +1,40 @@
-// import { Message } from "discord.js";
-// import KaikiCommand from "../../lib/Kaiki/KaikiCommand";
-//
-// export default class UpdateCommand extends KaikiCommand {
-//     constructor() {
-//         super("update", {
-//             aliases: ["update"],
-//             ownerOnly: true,
-//         });
-//     }
-//
-//     public async exec(message: Message): Promise<void> {
-//         //
-//     }
-// }
+import { execFile } from "child_process";
+import { EmbedBuilder, Message } from "discord.js";
+import path from "path";
+import KaikiCommand from "../../lib/Kaiki/KaikiCommand";
+import Utility from "../../lib/Utility";
+
+export default class UpdateCommand extends KaikiCommand {
+    constructor() {
+        super("update", {
+            aliases: ["update"],
+            ownerOnly: true,
+        });
+    }
+
+    public async exec(message: Message): Promise<void> {
+        execFile(path.join(path.resolve(), "..", "..", "..", "external", "update.sh"), async (error, stdout, stderr) => {
+            if (error) {
+                return message.channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle("Error occurred while updating")
+                            .setDescription(await Utility.codeblock(error.message))
+                            .withOkColor(message),
+                    ],
+                });
+            }
+
+            else {
+                return message.channel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(stderr)
+                            .setDescription(await Utility.codeblock(stdout))
+                            .withOkColor(message),
+                    ],
+                });
+            }
+        });
+    }
+}
