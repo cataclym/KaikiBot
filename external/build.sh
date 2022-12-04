8ac638f96c37adf3cdcf2b7d7cb80227aa9f0110
Cata <11278743+cataclym@users.noreply.github.com>
2022-12-04 15:20:19 +0100

    Fix: Made npm ci run build in foreground - Fixing update procedure

--- external/build.sh
+++ external/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 if command -v npm &> /dev/null; then
-  npm ci && exit 0 || exit 1;
+  npm ci --foreground-scripts 1>&2 && exit 0 || exit 1;
 fi
 
 echo "npm is not in your path!" 1>&2
