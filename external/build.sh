8fe493927f532e03605526c7838a1130e532e6c6
Cata <11278743+cataclym@users.noreply.github.com>
2022-12-05 01:35:29 +0100

    more update bash

--- external/build.sh
+++ external/build.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 
 if command -v npm &> /dev/null; then
-  npm ci --foreground-scripts 1>&2 && exit 0 || exit 1;
+  npm ci --foreground-scripts && exit 0 || exit 1;
 fi
 
-echo "npm is not in your path!" 1>&2
+echo "npm is not in your path!" >&2
 exit 1
