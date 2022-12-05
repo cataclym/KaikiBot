43e508696c2090d37023c7a1faa62b8dc1fa2dea
Cata <11278743+cataclym@users.noreply.github.com>
2022-12-05 01:23:14 +0100

    Changed: Redirect error to stderr, and quote all methods

--- external/update.sh
+++ external/update.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/bash
-currentTag=$(git describe)
+currentTag="$(git describe)"
 git fetch > /dev/null
-latestTag=$(git tag | sort -V | tail -1)
+latestTag="$(git tag | sort -V | tail -1)"
 
 if [[ "$latestTag" != "$currentTag" ]]; then
   echo "New version detected. Updating from ${currentTag} to ${latestTag}..."
-  git checkout "$latestTag" && currentTag=$(git describe)
+  git checkout "$latestTag" && currentTag="$(git describe)"
 
   if [[ "$latestTag" == "$currentTag" ]]; then
     echo "Successfully updated to ${latestTag}!" 1>&2
     exit 0
   else
-    echo "Error, unable to update..." 1>&2
+    echo "Error, unable to update..." >&2
     exit 1
   fi
 fi
