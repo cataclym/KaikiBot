1eb01df3d823f3156310ca5662e38699e2c3102e
Cata <11278743+cataclym@users.noreply.github.com>
2022-12-22 22:09:40 +0100

    This API is deprecated

--- src/lib/APIs/MikuAPI.ts
+++
@@ -1,6 +0,0 @@
-import fetch from "node-fetch";
-import KaikiUtil from "../Kaiki/KaikiUtil";
-
-export async function getMikuImage(): Promise<string | void> {
-    return (await KaikiUtil.handleToJSON(await (await fetch("https://miku-for.us/api/v2/random")).json()))["url"];
-}
