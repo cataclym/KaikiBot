98ff85c77d4e5a9fc03c33bf3d19ab8b1168c593
Cata <11278743+cataclym@users.noreply.github.com>
2023-02-21 19:52:09 +0100

    Removed deprecated guild features

--- src/lib/Types/Miscellaneous.ts
+++ src/lib/Types/Miscellaneous.ts
@@ -1,14 +1,2 @@
-import { Snowflake } from "discord.js";
-
 export type RegexpType = { match: RegExpMatchArray };
 export type RespType = { anime: string, character: string, quote: string };
-export type EmoteReactObjectType = { [keyWord: string]: Snowflake };
-
-export type TheseDoNotYetExist = "APPLICATION_COMMAND_PERMISSIONS_V2"
-    | "AUTO_MODERATION"
-    | "BOOSTING_TIERS_EXPERIMENT_MEDIUM_GUILD"
-    | "MEMBER_PROFILES"
-    | "NEW_THREAD_PERMISSIONS"
-    | "THREADS_ENABLED"
-    | "THREE_DAY_THREAD_ARCHIVE"
-    | "TEXT_IN_VOICE_ENABLED";
