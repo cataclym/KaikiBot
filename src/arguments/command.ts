a1ae29cd104d28fa6d099c290eb4f8b5618e7b27
Cata <11278743+cataclym@users.noreply.github.com>
2023-06-14 17:56:50 +0200

    Updated command argument's resolve strategy

--- src/arguments/command.ts
+++ src/arguments/command.ts
@@ -3,22 +3,37 @@ import KaikiCommand from "../lib/Kaiki/KaikiCommand";
 
 export class CommandArgument extends Argument<KaikiCommand> {
     public run(parameter: string, context: Argument.Context<KaikiCommand>): Argument.AwaitableResult<KaikiCommand> {
-        const result = <KaikiCommand>container.stores.get("commands")
+        const cmds = container.stores.get("commands");
+
+        const fullNameResult = cmds.find(k => {
+            const name = k.name.toLowerCase();
+            const param = parameter.toLowerCase();
+
+            if (name === param) return true;
+        });
+
+        const aliasResult = <KaikiCommand>container.stores.get("commands")
+            .aliases
+            .get(parameter);
+
+        const partialResult = cmds
+            .sort((firstValue, secondValue) => firstValue.name.length - secondValue.name.length)
             .find(k => {
                 const name = k.name.toLowerCase();
+                const param = parameter.toLowerCase();
 
-                return parameter
-                    .toLowerCase()
-                    .startsWith(name.slice(0, Math.max(parameter.length - 1, 1)));
+                return param
+                    .startsWith(name.slice(0, Math.max(param.length - 1, 1)));
             });
 
-        if (!result) {
+        if (!aliasResult && !fullNameResult && !partialResult) {
             return this.error({
                 message: "The provided argument could not be resolved to a command.",
                 parameter,
                 context,
             });
         }
-        return this.ok(result);
+
+        return this.ok(aliasResult || fullNameResult || partialResult);
     }
 }
